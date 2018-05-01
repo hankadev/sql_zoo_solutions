3965b6bd6b62504f5c84fde22553fde7ee8cc555
Hana Klingová <hana.klingova@gmail.com>
2018-05-01 16:29:25 +0200

    add exercise 9 to lesson 9

--- 9_self_JOIN.sql
+++ 9_self_JOIN.sql
@@ -52,3 +52,12 @@ FROM route a JOIN route b ON
   JOIN stops stopa ON (a.stop = stopa.id)
   JOIN stops stopb ON (b.stop = stopb.id)
 WHERE stopa.name='Craiglockhart' AND stopb.name = 'Tollcross'
+
+-- 9. Give a distinct list of the stops which may be reached from 'Craiglockhart' by taking one bus, including 'Craiglockhart' itself,
+-- offered by the LRT company. Include the company and bus no. of the relevant services.
+SELECT DISTINCT stopa.name, a.company, a.num
+FROM route a JOIN route b ON
+  (a.company = b.company AND a.num = b.num)
+  JOIN stops stopa ON (a.stop = stopa.id)
+  JOIN stops stopb ON (b.stop = stopb.id)
+WHERE stopa.name='Craiglockhart' OR stopb.name='Craiglockhart';
